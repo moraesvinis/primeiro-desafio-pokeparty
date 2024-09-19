526b9ad1f932a69eec36384a524beaf56b7da222
moraesvinis <sepamoraes@gmail.com>
2024-09-19 18:32:59 -0300

    first commit

---
+++ src/pokedex.js
@@ -0,0 +1,22 @@
+// Party Pokemon
+
+let nomesPokemon = ["Poochyena" , "Zigzagoon" , "Dragonite" , "Dragonite" , "Dragonite" , "Poochyena" , "Wurmple"]
+let timePokemon = [
+  ["Poochyena", "M", 2],
+  ["Zigzagoon", "F", 2],
+  ["Dragonite", "M", 5],
+  ["Dragonite", "F", 5],
+  ["Dragonite", "F", 5],
+  ["Poochyena", "F", 3],
+  ["Wurmple", "M", 2]
+]
+
+const sucesso = " cadastrado com sucesso!"
+
+console.log(timePokemon[0][0] + sucesso)
+console.log(timePokemon[1][0] + sucesso)
+console.log(timePokemon[2][0] + sucesso)
+console.log(timePokemon[3][0] + sucesso)
+console.log(timePokemon[4][0] + sucesso)
+console.log(timePokemon[5][0] + sucesso)
+console.log(timePokemon[6][0] + sucesso)
